b807f7047beae32f59b6d73c11d6d8023df98bfc
Schaked Halperin <shakkedhalperin@MacBook-Pro-3.local>
2015-12-08 02:21:45 -0800

    adding the training folder

---
+++ PleasePleasePleaseGitMeOuttaHere/CocoaSpeechSynthesisExample/SpeakingTextWindow.h
@@ -0,0 +1,23 @@
+/*
+ Copyright (C) 2015 Apple Inc. All Rights Reserved.
+ See LICENSE.txt for this sample’s licensing information
+ 
+ Abstract:
+ The main window hosting all the apps speech features.
+ */
+
+@import Cocoa;
+
+@interface SpeakingTextWindow : NSDocument
+@property (weak) IBOutlet NSButton *PhonButton;
+@property (weak) IBOutlet NSButton *t;
+@property (weak) IBOutlet NSButton *s;
+@property (weak) IBOutlet NSButton *r;
+@property (weak) IBOutlet NSButton *n;
+@property (weak) IBOutlet NSButton *IY;
+@property (weak) IBOutlet NSButton *d;
+@property (weak) IBOutlet NSButton *IH;
+@property (weak) IBOutlet NSButton *AX;
+
+@end
+
